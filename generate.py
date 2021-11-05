253c267a89e7545524bcd0db3859fdfa801e303e
Akila Mendis <akila@oneit.com.au>
2021-11-05 10:59:19 +0530

    monior fixes

--- generate.py
+++ generate.py
@@ -6,7 +6,7 @@ import os
 device = torch.device('cpu')
 netG = G()
 netG.load_state_dict(torch.load('trained_generator_weights.pt', map_location=device))
-num_of_imgs = 64
+num_of_imgs = 50
 vec = torch.randn((num_of_imgs, 16))
 with torch.no_grad():
     fake = netG(vec)
@@ -14,5 +14,7 @@ with torch.no_grad():
 directory='fakes'
 if not os.path.exists(directory):
     os.makedirs(directory)
+starting_num = 1
 for i in range(num_of_imgs):
-    vutils.save_image(fake.data[i], os.path.join(directory, f'aipoke.{i:02d}.png'), normalize=True)
+    vutils.save_image(fake.data[i], os.path.join(directory, f'aipoke{starting_num:02d}.png'), normalize=True)
+    starting_num = starting_num + 1
